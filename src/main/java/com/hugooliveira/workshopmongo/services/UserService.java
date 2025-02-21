eb05603bccd2f41c34a94eb6af7d2e1c71c66b16
Hugo <hugohenri777@gmail.com>
2025-02-20 22:34:09 -0300

    Atualização do usuario com PUT

--- src/main/java/com/hugooliveira/workshopmongo/services/UserService.java
+++ src/main/java/com/hugooliveira/workshopmongo/services/UserService.java
@@ -36,6 +36,22 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User entity) {
+		User newObj = repo.findById(entity.getId()).get();
+		updateData(newObj, entity);
+		return repo.save(newObj);
+		
+	}
+	
+	
+
+	private void updateData(User newObj, User entity) {
+		newObj.setName(entity.getName());
+		newObj.setEmail(entity.getEmail());
+		
+		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
